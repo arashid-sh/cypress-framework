1d687181df7e9aa6cd71226b15925288c346edff
Affan Rashid <arashidi@sidecarhealth.com>
2022-06-29 13:25:54 -0700

    adding more commands

--- cypress/e2e/todomvc.cy.js
+++ cypress/e2e/todomvc.cy.js
@@ -5,8 +5,13 @@ describe("Learning Cypress", () => {
     cy.visit("https://todomvc-app-for-testing.surge.sh/");
 
     cy.get(".new-todo", { timeout: 6000 }).type("Clean room{enter}");
-    cy.get(".toggle").click();
 
-    cy.contains("Clear completed").click();
+    cy.get("label").should("have.text", "Clean room");
+
+    cy.get(".toggle").should("not.be.checked");
+
+    // cy.get(".toggle").click();
+
+    // cy.contains("Clear completed").click();
   });
 });
